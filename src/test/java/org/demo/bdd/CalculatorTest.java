69a644b4397aa8a7c7b16d189f723ffd0038e293
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-12-06 17:58:18 +0100

    Added json report

--- src/test/java/org/demo/bdd/CalculatorTest.java
+++ src/test/java/org/demo/bdd/CalculatorTest.java
@@ -6,6 +6,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:org/demo/bdd/calculator.feature" })
+@CucumberOptions(features = { "classpath:org/demo/bdd/calculator.feature" },
+        plugin = { "json", "json:target/cucumber-reports/Cucumber.json" },
+        monochrome = true)
 public class CalculatorTest implements En {
 }
